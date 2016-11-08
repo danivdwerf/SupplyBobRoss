b77bee61aa12b0567a1f773766d191a9d5ea98cb
Danivdwerf <danivdwerf.com>
2016-11-08 08:49:09 +0100

    merged version

---
+++ Assets/_Scripts/Paint/BucketGravity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BucketGravity : MonoBehaviour 
+{
+	private float gravity;
+	private Rigidbody rigidBody;
+	private void Start()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+		gravity = -10f;
+	}
+
+	private void FixedUpdate()
+	{
+		rigidBody.AddForce (0f, gravity, 0f);
+	}
+}

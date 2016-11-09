6b752ed17b536ef36c221dad9a988d349dc51a4d
Danivdwerf <danivdwerf.com>
2016-11-09 19:42:54 +0100

    added start/instructionsScreen

---
+++ Assets/_Scripts/StartScreen/ButtonHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour 
+{
+	[SerializeField] private Button startButton;
+	[SerializeField] private Button instructionButton;
+	[SerializeField] private Button quitButton;
+	[SerializeField] private Button backButton;
+
+	private SceneSwitcher sceneSwitch;
+	private HandleUI ui;
+	private void Start()
+	{
+		sceneSwitch = GetComponent<SceneSwitcher> ();
+		ui = GetComponent<HandleUI> ();
+
+		startButton.onClick.AddListener (delegate(){sceneSwitch.ChangeScene(1);});
+		instructionButton.onClick.AddListener (delegate(){ui.ShowInstructions(true);});
+		quitButton.onClick.AddListener (delegate(){Application.Quit();});
+		backButton.onClick.AddListener(delegate(){ui.ShowInstructions(false);}); 
+	}
+}
---
+++ Assets/_Scripts/StartScreen/HandleUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleUI : MonoBehaviour 
+{
+	[SerializeField] private GameObject instructionPanel;
+
+	private void Start()
+	{
+		instructionPanel.SetActive (false);
+	}
+
+	public void ShowInstructions(bool state)
+	{
+		instructionPanel.SetActive (state);
+	}
+}
